6367299a178cfeef89d309f3f44dc0e4ff8bb4a9
luhuibo <luhuibo306@foxmail.com>
2019-07-17 11:32:25 +0800

    【更新】pip install -U jupyter 安装，脚本每次都更新最新的

--- #start_jupyter_notebook.bat
+++ #start_jupyter_notebook.bat
@@ -1,6 +1,6 @@
 rem install jupyter notebook in windows10
 rem pip install jupyter
-pip install jupyter
+pip install -U jupyter
 rem -- start_jupyter_notebook.bat ---
 rem open jupyter notebook in chrome
 call "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8888/tree
